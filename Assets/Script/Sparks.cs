58a08e9feabb5662c4dda4339fcca4ee93bb2927
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2021-11-18 09:32:44 +0900

    effect処理　エフェクトを出すためのスクリプトが抜けてた

---
+++ Assets/Script/Sparks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sparks : MonoBehaviour
+{
+    public ParticleSystem spk;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            spk.Play();
+        }
+    }
+}

327a5eadc6f1593b68de934037e8fe3245c5214c
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2021-11-18 13:12:02 +0900

    effect処理　壁に当たった時のエフェクトが出るようにできた

--- Assets/Script/Sparks.cs
+++ Assets/Script/Sparks.cs
@@ -6,10 +6,17 @@ public class Sparks : MonoBehaviour
 {
     public ParticleSystem spk;
 
-    void OnCollisionEnter(Collision other)
+    void Update()
     {
-        if(other.gameObject.tag == "Ball")
+        Debug.Log("呼ばれた");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("触った");
+        if (collision.gameObject.tag == "Wall")
         {
+            spk.transform.position = collision.contacts[0].point;
             spk.Play();
         }
     }
